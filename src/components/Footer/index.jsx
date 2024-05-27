544b132bf3387430c43ec99636aa3354f01b85c9
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-05-27 09:39:46 +0200

    social media

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,15 @@
 import './style.scss';
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
+
 
 function Footer () {
     return (
         <footer>
+            <div className='link-social-media'>
+                <a href='https://github.com/Noalune12' target='blank'><IoLogoLinkedin/></a>
+                <a href='https://www.linkedin.com/in/lou-anne-buisson-b855b19b/' target='blank'><FaGithub/></a>
+            </div>
             <p>© 2024 Lou-Anne Buisson. All rights reserved</p>
             <a href="https://www.flaticon.com/free-icons/coding" title="coding icons" target='blank'>Coding icons created by edt.im - Flaticon</a>
         </footer>
